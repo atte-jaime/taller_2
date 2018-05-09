f2818f4a9fd49cd6db0a3cdb93aeab83e587983b
tetolalo <jaimeal.99@hotmail.com>
2018-05-09 08:45:35 -0500

    Data base added

---
+++ index.js
@@ -0,0 +1,44 @@
+const MongoClient = require('mongodb').MongoClient,
+    express = require('express'),
+    engines = require('consolidate');
+
+var app = express(),
+    db;
+
+app.engine('hbs', engines.handlebars);
+
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+// Conectarse a Base de Datos
+MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+    if (err) throw err;
+
+    db = client.db('productos');
+
+    // Iniciar servidor
+    app.listen(1234);
+});
+
+app.get('/', (req, res) => {
+    
+    var prod = db.collection('productos')
+        .find();
+
+    if (req.query.marca)
+        prod.filter({
+            marca: req.query.marca
+        });
+
+    if (req.query.modelo)
+        prod.filter({
+            modelo: req.query.modelo
+        });
+
+    prod.toArray((err, result) => {
+        console.log('hola servidor')
+        res.render('index', {
+            productos: result
+        });
+    })
+});
